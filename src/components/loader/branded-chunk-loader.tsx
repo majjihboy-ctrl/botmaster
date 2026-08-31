6e53edc95211a833b699f7a8f74285c9c0606210
majjihboy-ctrl <majjihboy@gmail.com>
2026-08-31 12:56:35 +0300

    Update branded-chunk-loader.tsx

--- src/components/loader/branded-chunk-loader.tsx
+++ src/components/loader/branded-chunk-loader.tsx
@@ -9,8 +9,21 @@ type TBrandedChunkLoader = {
 export default function BrandedChunkLoader({ message }: TBrandedChunkLoader) {
     return (
         <div className='branded-chunk-loader'>
-            <BrandedSpinner />
-            <div className='branded-chunk-loader__message'>{message ?? localize('Loading...')}</div>
+            <div className='branded-chunk-loader__ambient branded-chunk-loader__ambient--one' />
+            <div className='branded-chunk-loader__ambient branded-chunk-loader__ambient--two' />
+            <div className='branded-chunk-loader__panel'>
+                <BrandedSpinner />
+                <div className='branded-chunk-loader__copy'>
+                    <div className='branded-chunk-loader__topline'>
+                        <span className='branded-chunk-loader__dot' />
+                        {localize('Botmaster')}
+                    </div>
+                    <div className='branded-chunk-loader__message'>{message ?? localize('Loading...')}</div>
+                    <div className='branded-chunk-loader__progress'>
+                        <div className='branded-chunk-loader__progress-fill' />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
