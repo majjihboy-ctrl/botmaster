a1d56bd76af3f063f643b8df4b4ecfe29f03767b
majjihboy-ctrl <majjihboy@gmail.com>
2026-09-17 14:52:02 +0300

    fix: do not show RunPanel on AutoTrade/Custom Pro tab

--- src/components/run-panel/run-panel.tsx
+++ src/components/run-panel/run-panel.tsx
@@ -268,7 +268,7 @@ const RunPanel = observer(() => {
     const { statistics } = transactions;
     const { active_tour, active_tab } = dashboard;
     const { total_payout, total_profit, total_stake, won_contracts, lost_contracts, number_of_runs } = statistics;
-    const { BOT_BUILDER, CHART, ANALYSIS_TOOL, FREE_BOTS, AUTOTRADE } = DBOT_TABS;
+    const { BOT_BUILDER, CHART, ANALYSIS_TOOL, FREE_BOTS } = DBOT_TABS;
 
     React.useEffect(() => {
         onMount();
@@ -311,7 +311,7 @@ const RunPanel = observer(() => {
         />
     );
 
-    const show_run_panel = [BOT_BUILDER, CHART, ANALYSIS_TOOL, FREE_BOTS, AUTOTRADE].includes(active_tab) || active_tour;
+    const show_run_panel = [BOT_BUILDER, CHART, ANALYSIS_TOOL, FREE_BOTS].includes(active_tab) || active_tour;
     if ((!show_run_panel && isDesktop) || active_tour === 'bot_builder') return null;
 
     return (
